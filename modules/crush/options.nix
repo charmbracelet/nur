b7122aaba209912767d44a7acee7ee719f00611f
Malik <malik@desktop.ke>
2025-10-29 22:56:03 +0100

    crush: add overridable programs.crush.package option
    Add programs.crush.package (defaulting to callPackage ../../pkgs/crush {}) and switch options to accept pkgs (inherit lib from pkgs). Refactor nixos/home-manager modules to use cfg = config.programs.crush and reference cfg.package and cfg.settings. Primary goal: make the crush package selectable; no functional change by default.

--- modules/crush/options.nix
+++ modules/crush/options.nix
@@ -1,8 +1,16 @@
-{ lib }:
+{ pkgs }:
 
-{
+let
+ inherit (pkgs) lib;
+in {
   programs.crush = {
     enable = lib.mkEnableOption "Enable crush";
+    package = lib.mkOption {
+      default = (pkgs.callPackage ../../pkgs/crush {});
+      defaultText = "crush";
+      description = "Package to use for crush";
+    };
+
     settings = lib.mkOption {
       type = lib.types.submodule {
         options = {
