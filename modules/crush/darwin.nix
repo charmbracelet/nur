66222401feda7ec4b88a05400eb5ae07e76a8203
Dan Xin <davinci42.cn@gmail.com>
2026-02-07 19:22:18 +0800

    feat: crush support darwin modules
    Signed-off-by: Dan Xin <davinci42.cn@gmail.com>

---
+++ modules/crush/darwin.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.programs.crush;
+in {
+  options = import ./options { inherit pkgs; };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+    environment.etc."crush/crush.json" = lib.mkIf (cfg.settings != {}) {
+      text = builtins.toJSON cfg.settings;
+    };
+  };
+}
